0476f97beee914e961b9a0fcaa3dc1372757c46f
Milo Rastgoo <hi@informagician.com>
2020-02-11 15:57:56 -0500

    blog router renamed to posts router. first post end point created

---
+++ data/router/posts-router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const Posts = require('../db');
+
+const router = express.Router();
+
+router.post('/', (req,res) => {
+    
+    if(!req.body.title || ! req.body.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    } else {
+        Posts.insert(req.body).then(post => {
+            res.status(201).json(post)
+        }).catch(err => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+        })
+    }
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const postsRouter = require('./data/router/posts-router.js')
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/posts', postsRouter)
 
 server.get('/', (req,res) => {
     res.send('<h1>Things are working!</h1>')
