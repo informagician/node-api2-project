fe23042a866a514f48b41dbdc888401a1b707e19
Milo Rastgoo <hi@informagician.com>
2020-02-11 17:13:08 -0500

    find comments by post id

--- data/router/posts-router.js
+++ data/router/posts-router.js
@@ -64,4 +64,23 @@ router.get(`/:id`, (req,res) => {
         })
 })
 
+// When the client makes a GET request to /api/posts/:id/comments
+router.get(`/:id/comments`, (req,res) => {
+    const {id} = req.params;
+    Posts.findById(id)
+    .then(posts => {
+        if(posts.length === 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            Posts.findPostComments(id)
+            .then(comments => {
+                res.status(200).json(comments)
+            })
+            .catch(err => {
+                res.status(500).json({ error: "The comments information could not be retrieved." })
+            })
+        }
+    })
+})
+
 module.exports = router;
