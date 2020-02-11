12947389ce6ba69ed60f67aeb7eecd7edbeb0d07
Milo Rastgoo <hi@informagician.com>
2020-02-11 18:45:28 -0500

    stretch delete works

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,20 @@ const App = () => {
       })
       .catch(err => console.log(err))
   },[])
+
+  const handleDelete = id => {
+    axios.delete(`http://localhost:5000/api/posts/${id}`)
+    .then(res => {
+      console.log(res);
+      axios.get('http://localhost:5000/api/posts')
+      .then(res => {
+        console.log(res)
+        setPosts(res.data)
+      })
+      .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+  }
   return (
     <>
       <div>
@@ -22,7 +36,7 @@ const App = () => {
           <h3>{post.title}</h3>
           <p>{post.contents}</p>
           <p>{post.created_at}</p>
-          <span>[Delete]</span><span>[Edit]</span>
+          <span onClick={() => handleDelete(post.id)}>[Delete]</span><span>[Edit]</span>
           <hr/>
           </>
         ))}
